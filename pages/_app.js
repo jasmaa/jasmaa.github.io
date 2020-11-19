6cf55df12428bcf1e25c2efec7c17b5db6fd15cf
jasmaa <jasonmaa3955@gmail.com>
2020-11-19 14:20:57 -0500

    Add code highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
+import hljs from 'highlight.js/lib/core';
+import go from 'highlight.js/lib/languages/go';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "animate.css/animate.min.css";
+import 'animate.css/animate.min.css';
+import 'highlight.js/styles/github.css';
 import '@styles/globals.css';
 
+// Register more languages as needed
+hljs.registerLanguage('go', go);
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Highlight all multiline code blocks
+    document.querySelectorAll('pre > code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  });
   return <Component {...pageProps} />
 }
 
