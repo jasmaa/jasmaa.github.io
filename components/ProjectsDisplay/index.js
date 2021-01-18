517bc1c8b667faf31b890fb135d343d01ee9e4e6
jasmaa <jasonmaa3955@gmail.com>
2021-01-17 21:54:01 -0500

    Add images to projects

--- components/ProjectsDisplay/index.js
+++ components/ProjectsDisplay/index.js
@@ -8,20 +8,23 @@ import { faCode, faLink } from '@fortawesome/free-solid-svg-icons';
  */
 export default function ProjectsDisplay({ items }) {
   return (
-    <div className="grid md:grid-cols-2 gap-10 grid-cols-flow my-5">
+    <div className="grid lg:grid-cols-2 gap-10 grid-cols-flow my-5">
       {items.map(item => (
-        <div key={item.name} className="rounded shadow-xl bg-white p-5 transform transition hover:scale-110 my-5">
-          <div>
-            <h3 className="text-3xl">{item.name}</h3>
-            <p className="py-4">{item.description}</p>
-          </div>
-          <div className="flex justify-end">
-            {item.sourceURL
-              ? <a className="ml-3" href={item.sourceURL}><FontAwesomeIcon icon={faCode} size="lg" /></a>
-              : null}
-            {item.siteURL
-              ? <a className="ml-3" href={item.siteURL}><FontAwesomeIcon icon={faLink} size="lg" /></a>
-              : null}
+        <div key={item.name} className="flex flex-col rounded shadow-xl bg-gray-200 my-5">
+          <img className="w-full" src={item.imageURL} />
+          <div className="h-full flex flex-col justify-between p-5">
+            <div>
+              <h3 className="text-3xl">{item.name}</h3>
+              <p className="py-4">{item.description}</p>
+            </div>
+            <div className="flex justify-end mt-5">
+              {item.sourceURL
+                ? <a className="ml-3" href={item.sourceURL}><FontAwesomeIcon icon={faCode} size="lg" /></a>
+                : null}
+              {item.siteURL
+                ? <a className="ml-3" href={item.siteURL}><FontAwesomeIcon icon={faLink} size="lg" /></a>
+                : null}
+            </div>
           </div>
         </div>
       ))}
