cfc5c4ada8314664cd8484146efd8ebae662f75b
jasmaa <jasonmaa3955@gmail.com>
2021-04-11 12:39:23 -0400

    Add maze contest post

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import hljs from 'highlight.js/lib/core';
 import go from 'highlight.js/lib/languages/go';
+import r from 'highlight.js/lib/languages/r';
 
 import 'animate.css/animate.min.css';
 import 'highlight.js/styles/github.css';
@@ -9,6 +10,7 @@ import '@styles/globals.css';
 
 // Register more languages as needed
 hljs.registerLanguage('go', go);
+hljs.registerLanguage('r', r);
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
